ad5dfea2fc6180198898cb629c56af17f9039828
IvanStojanov <ivan.stojanov1990@gmail.com>
2018-02-21 23:21:44 +0100

    stream Acceleration and GYRO, as in the official docmentation, next: stream them as in examle app + combine the streams into one

--- app/src/main/java/org/andresoviedo/app/model3D/view/ModelActivity.java
+++ app/src/main/java/org/andresoviedo/app/model3D/view/ModelActivity.java
@@ -14,26 +14,53 @@ import org.andresoviedo.dddmodel2.R;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.mbientlab.metawear.Data;
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.Route;
+import com.mbientlab.metawear.Subscriber;
+import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.builder.RouteBuilder;
+import com.mbientlab.metawear.builder.RouteComponent;
+import com.mbientlab.metawear.builder.filter.Comparison;
+import com.mbientlab.metawear.builder.filter.ThresholdOutput;
+import com.mbientlab.metawear.builder.function.Function1;
+import com.mbientlab.metawear.data.Acceleration;
+import com.mbientlab.metawear.data.AngularVelocity;
+import com.mbientlab.metawear.module.Accelerometer;
+import com.mbientlab.metawear.module.Debug;
+import com.mbientlab.metawear.module.GyroBmi160;
+import com.mbientlab.metawear.module.Logging;
+
+import bolts.Continuation;
+import bolts.Task;
 
 /**
  * This activity represents the container for our 3D viewer.
  *
  * @author andresoviedo
  */
-public class ModelActivity extends Activity {
+public class ModelActivity extends Activity implements ServiceConnection {
 
 	private static final int REQUEST_CODE_OPEN_FILE = 1000;
 
@@ -57,6 +84,18 @@ public class ModelActivity extends Activity {
 	private SceneLoader scene;
 
 	private Handler handler;
+
+
+	private static final String LOG_TAG_ACC = "ModelActivityTagACC";
+	private static final String LOG_TAG_GYRO = "ModelActivityTagGYRO";
+	private MetaWearBoard mwBoard;
+	private Accelerometer accelerometer;
+	private GyroBmi160 gyroBmi160;
+	private Debug debug;
+	private Logging logging;
+
+	BtleService.LocalBinder serviceBinder;
+	BluetoothDevice btDevice1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -123,6 +162,12 @@ public class ModelActivity extends Activity {
 		Utils.printTouchCapabilities(getPackageManager());
 
 		setupOnSystemVisibilityChangeListener();
+
+		getApplicationContext().bindService(new Intent(this, BtleService.class), this, Context.BIND_AUTO_CREATE);
+
+
+		//findViewById(R.id.btn_start).bringToFront();
+		//findViewById(R.id.btn_stop).bringToFront();
 	}
 
 	/**
@@ -318,6 +363,116 @@ public class ModelActivity extends Activity {
 					Toast.makeText(getApplicationContext(), "Result when loading texture was '" + resultCode + "'",
 							Toast.LENGTH_SHORT).show();
 				}
+		}
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder service) {
+		serviceBinder = (BtleService.LocalBinder) service;
+
+		String mwMacAddress= "C1:5F:8D:92:E5:07";   ///< Put your board's MAC address here
+		BluetoothManager btManager= (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+		btDevice1= btManager.getAdapter().getRemoteDevice(mwMacAddress);
+
+		mwBoard= serviceBinder.getMetaWearBoard(btDevice1);
+		mwBoard.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
+			@Override
+			public Task<Route> then(Task<Void> task) throws Exception {
+
+				//accelerometer
+				accelerometer = mwBoard.getModule(Accelerometer.class);
+				accelerometer.configure()
+						.odr(50f)
+						.commit();
+
+				//Log.i(LOG_TAG_ACC, "Actual accelerometer Odr = " + accelerometer.getOdr());
+				//Log.i(LOG_TAG_ACC, "Actual accelerometer Range = " + accelerometer.getRange());
+
+				accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
+					@Override
+					public void configure(RouteComponent source) {
+						source.stream(new Subscriber() {
+							@Override
+							public void apply(Data data, Object... env) {
+								Log.i(LOG_TAG_ACC, data.value(Acceleration.class).toString());
+							}
+						});
+					}
+				});
+
+				//gyro
+				gyroBmi160 = mwBoard.getModule(GyroBmi160.class);
+				gyroBmi160.configure()
+						.odr(GyroBmi160.OutputDataRate.ODR_50_HZ)
+						.range(GyroBmi160.Range.FSR_2000)
+						.commit();
+
+				gyroBmi160.angularVelocity().addRouteAsync(new RouteBuilder() {
+					@Override
+					public void configure(RouteComponent source) {
+						source.stream(new Subscriber() {
+							@Override
+							public void apply(Data data, Object ... env) {
+								Log.i(LOG_TAG_GYRO, data.value(AngularVelocity.class).toString());
+							}
+						});
+					}
+				}).continueWith(new Continuation<Route, Void>() {
+					@Override
+					public Void then(Task<Route> task) throws Exception {
+						gyroBmi160.angularVelocity();
+						gyroBmi160.start();
+						return null;
+					}
+				});
+
+				return null;
+			}
+		}).continueWith(new Continuation<Route, Void>() {
+
+			@Override
+			public Void then(Task<Route> task) throws Exception {
+				accelerometer.acceleration().start();
+				accelerometer.start();
+				return null;
+			}
+		});
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		stopAcceleration();
+	}
+
+	@Override
+	public void onBackPressed() {
+		stopAcceleration();
+		super.onBackPressed();
+	}
+
+	public void stopAcceleration(){
+		if(accelerometer != null){
+			accelerometer.stop();
+			accelerometer.acceleration().stop();
+			Log.i(LOG_TAG_ACC, "accelerometer stopped");
+		}
+		if(logging != null){
+			logging.stop();
+			logging.downloadAsync().continueWith(new Continuation<Void, Void>() {
+				@Override
+				public Void then(Task<Void> task) throws Exception {
+					Log.i(LOG_TAG_ACC, "Log download complete");
+					return null;
+				}
+			});
+		}
+		if(gyroBmi160 != null){
+			gyroBmi160.stop();
+			(gyroBmi160.packedAngularVelocity() == null ?
+					gyroBmi160.packedAngularVelocity() :
+					gyroBmi160.angularVelocity()
+			).stop();
+			Log.i(LOG_TAG_GYRO, "gyroBmi160 stopped");
 		}
 	}
 }
